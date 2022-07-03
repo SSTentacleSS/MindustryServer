0512e73e83328147aa84cb09c692bbe7033ee8d7
SSTentacleSS <ss_tentacle_ss@zohomail.eu>
2022-07-03 18:48:11 +0300

    Cleanup

--- src/java/mindustry/server/command/CommandsRegistry.java
+++ src/java/mindustry/server/command/CommandsRegistry.java
@@ -2,16 +2,12 @@ package mindustry.server.command;
 
 import arc.util.CommandHandler;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import mindustry.server.log.Colors;
 import mindustry.server.utils.Pipe;
 import org.jline.console.ArgDesc;
 import org.jline.console.CmdDesc;
-import org.jline.utils.AttributedString;
-import org.jline.utils.AttributedStyle;
 
 public class CommandsRegistry extends CommandHandler {
 
@@ -43,28 +39,15 @@ public class CommandsRegistry extends CommandHandler {
 			Pipe
 				.apply(parameter.name)
 				.pipe(text -> text.concat(parameter.variadic ? "..." : ""))
-				.pipe(text -> parameter.optional ? "[" + text + "]" : text)
+				.pipe(
+					text ->
+						parameter.optional ? "[" + text + "]" : "<" + text + ">"
+				)
 				.pipe(text -> parameters.add(text));
 		}
 
 		registeredCommands.add(command);
-		tailTips.put(
-			command.text,
-			new CmdDesc(
-				Arrays.asList(
-					Pipe
-						.apply(command.description)
-						.pipe(
-							Colors::applyStyle,
-							AttributedStyle.BOLD.foreground(255, 15, 15)
-						)
-						.pipe(text -> new AttributedString(text))
-						.result()
-				),
-				ArgDesc.doArgNames(parameters),
-				new HashMap<>()
-			)
-		);
+		tailTips.put(command.text, new CmdDesc(ArgDesc.doArgNames(parameters)));
 	}
 
 	@Override
